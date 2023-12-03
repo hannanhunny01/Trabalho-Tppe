a33350db8ee95dacff3f4eef7a5b4cb067736b8a
LeviQ27 <leviqueiroz647@gmail.com>
2023-12-03 20:07:57 -0300

    write class Transacao

---
+++ Models/Transacao.py
@@ -0,0 +1,9 @@
+class Transacao:
+    def __init__(self, produto, quantidade, fornecedor, loja):
+        self.produto = produto
+        self.quantidade = quantidade
+        self.fornecedor = fornecedor
+        self.loja = loja
+
+if __name__ == '__main__':
+    pass
