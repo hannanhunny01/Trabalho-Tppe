1d55c285b48ab16fde35765d62e68692141568d5
Josué Teixeira <zjosuez@outlook.com>
2023-12-03 13:15:08 -0300

    test cadastrar estoque

---
+++ Teste/testeEstoque.py
@@ -0,0 +1,18 @@
+import sys
+import unittest
+sys.path.append('Models')
+from Produto import Produto
+from Estoque import Estoque
+
+class TestEstoque(unittest.TestCase):
+    def setUp(self):
+        self.produto = Produto("Produto Teste", "123456789", 10.0, 20.0, "Fornecedor Teste", "Categoria Teste")
+        
+
+    def test_cadastrar_estoque(self):
+        self.estoque = Estoque(self.produto, 10, "2022-01-01", "2022-01-31", "entrada")
+        self.estoque.cadastrar_estoque()
+        self.assertEqual(len(self.estoque.estoque_cadastrado), 1)
+
+if __name__ == '__main__':
+    unittest.main()
