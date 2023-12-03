8ead887fe44ce742a6be9fd6aa733f7b7fe8cca8
heitormsb <heitormsb@gmail.com>
2023-12-02 23:14:49 -0300

    triangulação testProduto

--- Teste/testProduto.py
+++ Teste/testProduto.py
@@ -7,6 +7,8 @@ class TestProduto(unittest.TestCase):
 
     def test_init_produto(self):
         produto = Produto(descricao="Camiseta", codigo_barras="1234567890", custo=10.0, preco_venda=20.0, fornecedor="Fornecedor A", categoria="Roupa")
+        produto2 = Produto(descricao="Arroz", codigo_barras="0987654321", custo=15.0, preco_venda=25.0, fornecedor="Fornecedor B", categoria="Alimento")
+
 
         self.assertEqual(produto.descricao, "Camiseta")
         self.assertEqual(produto.codigo_barras, "1234567890")
@@ -14,6 +16,13 @@ class TestProduto(unittest.TestCase):
         self.assertEqual(produto.preco_venda, 20.0)
         self.assertEqual(produto.fornecedor, "Fornecedor A")
         self.assertEqual(produto.categoria, "Roupa")
+
+        self.assertEqual(produto2.descricao, "Arroz")
+        self.assertEqual(produto2.codigo_barras, "0987654321")
+        self.assertEqual(produto2.custo, 15.0)
+        self.assertEqual(produto2.preco_venda, 25.0)
+        self.assertEqual(produto2.fornecedor, "Fornecedor B")
+        self.assertEqual(produto2.categoria, "Alimento")
 
 if __name__ == '__main__':
     unittest.main()
