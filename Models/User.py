30e7e59f4fba76e8cc9da554e84fe7561c4dee10
hannanhunny01 <hannanhoney5000@gmail.com>
2023-12-02 23:23:45 -0300

    writing and testing Methods got Gerente And Funcionario

--- Models/User.py
+++ Models/User.py
@@ -8,6 +8,7 @@ class Usuario:
 
     def exibir_informacoes(self):
         return f"Nome: {self.nome}\nTelefone: {self.telefone}\nEmail: {self.email}"
+    
 
 
 
@@ -19,6 +20,25 @@ class Gerente(Usuario):
 
     def exibir_informacoes_gerente(self):
         return f"{super().exibir_informacoes()}\nCPF: {self.cpf}\nLojas: {', '.join(self.lojas)}"
+    
+    def listar_gerentes(gerentes):
+        return gerentes
+
+    def salvar_gerente(self, gerentes):
+        gerentes.append(self)
+        return gerentes
+
+    def atualizar_gerente(self, gerentes):
+        for i, gerente in enumerate(gerentes):
+            if gerente.email == self.email:
+                gerentes[i] = self
+                break
+        return gerentes
+
+    @staticmethod
+    def excluir_gerente(email, gerentes):
+        gerentes = [gerente for gerente in gerentes if gerente.email != email]
+        return gerentes
 
 
 class Funcionario(Usuario):
@@ -29,3 +49,22 @@ class Funcionario(Usuario):
 
     def exibir_informacoes_funcionario(self):
         return f"{super().exibir_informacoes()}\nCPF: {self.cpf}\nLoja: {self.loja}"
+    
+    def listar_funcionarios(funcionarios):
+        return funcionarios
+
+    def salvar_funcionario(self, funcionarios):
+        funcionarios.append(self)
+        return funcionarios
+
+    def atualizar_funcionario(self, funcionarios):
+        for i, funcionario in enumerate(funcionarios):
+            if funcionario.email == self.email:
+                funcionarios[i] = self
+                break
+        return funcionarios
+
+    @staticmethod
+    def excluir_funcionario(email, funcionarios):
+        funcionarios = [funcionario for funcionario in funcionarios if funcionario.email != email]
+        return funcionarios
