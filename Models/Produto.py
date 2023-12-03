47447863db4cd8195564e9c9f247d9c8dcbfb27c
heitormsb <heitormsb@gmail.com>
2023-12-02 23:11:53 -0300

    write Produto class

---
+++ Models/Produto.py
@@ -0,0 +1,11 @@
+class Produto:
+    def __init__(self, descricao, codigo_barras, custo, preco_venda, fornecedor, categoria):
+        self.descricao = descricao
+        self.codigo_barras = codigo_barras
+        self.custo = custo
+        self.preco_venda = preco_venda
+        self.fornecedor = fornecedor
+        self.categoria = categoria
+
+if __name__ == '__main__':
+    pass
