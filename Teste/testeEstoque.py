660fce24a78690bc202e2e0c7746ab054759b578
Josué Teixeira <zjosuez@outlook.com>
2023-12-03 13:25:53 -0300

    test remover estoque

--- Teste/testeEstoque.py
+++ Teste/testeEstoque.py
@@ -19,10 +19,21 @@ class TestEstoque(unittest.TestCase):
         self.assertEqual(len(self.estoque.estoque_cadastrado), 3)
         
     def test_verificar_estoque(self):
-        self.estoque = Estoque(self.produto, 10, "2022-01-01", "2022-01-31", "entrada")
+        self.estoque = Estoque(self.produto, 8, "2022-01-01", "2022-01-31", "entrada")
         self.estoque.cadastrar_estoque()
         quantity = self.estoque.verificar_estoque("123456789")
-        self.assertEqual(quantity, 10)
+        self.assertEqual(quantity, 8)
+
+    def test_remover_estoque(self):
+        self.estoque = Estoque(self.produto, 10, "2022-01-01", "2022-01-31", "entrada")
+        self.estoque.cadastrar_estoque()
+        initial_quantity = self.estoque.verificar_estoque("123456789")
+        qnt_remover = 2
+        self.estoque.remover_estoque("123456789", qnt_remover)
+        final_quantity = self.estoque.verificar_estoque("123456789")
+        self.assertEqual(final_quantity, initial_quantity - qnt_remover)
+        if qnt_remover >= initial_quantity:
+            self.assertNotIn("123456789", [estoque["produto"].codigo_barras for estoque in self.estoque.estoque_cadastrado])
 
 if __name__ == '__main__':
     unittest.main()
