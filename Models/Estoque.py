868797e51a73747c66e524241fb562365b0a7ce8
Josué Teixeira <zjosuez@outlook.com>
2023-12-03 13:26:15 -0300

    writhing method remover estoque

--- Models/Estoque.py
+++ Models/Estoque.py
@@ -23,3 +23,11 @@ class Estoque:
                 if estoque["Produto"].codigo_barras == codigo_barras:
                     return estoque["Quantidade"]
             return 0
+
+    def remover_estoque(self, codigo_barras, qnt_remover):
+        for estoque in self.estoque_cadastrado:
+            if estoque["Produto"].codigo_barras == codigo_barras:
+                estoque["Quantidade"] -= qnt_remover
+                if estoque["Quantidade"] <= 0:
+                    self.estoque_cadastrado.remove(estoque)
+                break
