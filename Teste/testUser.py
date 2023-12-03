559a155efe14c0877a022203bf9bb32ca77730b6
hannanhunny01 <hannanhoney5000@gmail.com>
2023-12-02 23:26:00 -0300

    Writing methods test for User

--- Teste/testUser.py
+++ Teste/testUser.py
@@ -23,11 +23,37 @@ class TestUsuario(unittest.TestCase):
         self.assertEqual(usuario.exibir_informacoes(), "Nome: Abdul\nTelefone: 123456789\nEmail: abdul@email.com")
         # Note que a senha não é exibida por questões de segurança
     
+    def test_criar_usuario(self):
+        usuario = Usuario("Usuario1", "123456789", "usuario1@example.com", "senha123")
+        self.assertEqual(usuario.nome, "Usuario1")
+        self.assertEqual(usuario.telefone, "123456789")
+        self.assertEqual(usuario.email, "usuario1@example.com")
+        self.assertEqual(usuario.senha, "senha123")
+
+    def test_listar_usuarios(self):
+        usuarios = [Usuario("Usuario1", "123456789", "usuario1@example.com", "senha123"),
+                    Usuario("Usuario2", "987654321", "usuario2@example.com", "senha456")]
+        self.assertEqual(Usuario.listar_usuarios(usuarios), usuarios)
+
+    def test_salvar_usuario(self):
+        usuarios = []
+        usuario = Usuario("Usuario1", "123456789", "usuario1@example.com", "senha123")
+        usuarios = usuario.salvar_usuario(usuarios)
+        self.assertEqual(len(usuarios), 1)
+
+    def test_atualizar_usuario(self):
+        usuarios = [Usuario("Usuario1", "123456789", "usuario1@example.com", "senha123")]
+        novo_usuario = Usuario("Usuario1", "123456789", "usuario1@example.com", "senha456")
+        usuarios = novo_usuario.atualizar_usuario(usuarios)
+        self.assertEqual(usuarios[0].senha, "senha456")
+
+    def test_excluir_usuario(self):
+        usuarios = [Usuario("Usuario1", "123456789", "usuario1@example.com", "senha123")]
+        email = "usuario1@example.com"
+        usuarios = Usuario.excluir_usuario(email, usuarios)
+        self.assertEqual(len(usuarios), 0)
     
 
 if __name__ == '__main__':
     unittest.main()
 
-
-if __name__ == '__main__':
-    unittest.main()
