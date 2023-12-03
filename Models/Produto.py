11be7f1eab0cfca5163bb7b794e8e8f39225eabe
heitormsb <heitormsb@gmail.com>
2023-12-02 23:21:10 -0300

    write cadastrar produto

--- Models/Produto.py
+++ Models/Produto.py
@@ -1,4 +1,6 @@
 class Produto:
+    produtos_cadastrados = [] 
+
     def __init__(self, descricao, codigo_barras, custo, preco_venda, fornecedor, categoria):
         self.descricao = descricao
         self.codigo_barras = codigo_barras
@@ -6,6 +8,21 @@ class Produto:
         self.preco_venda = preco_venda
         self.fornecedor = fornecedor
         self.categoria = categoria
+    
+    def cadastrar_produto(self):
+        produto_info = {
+            "descricao": self.descricao,
+            "codigo_barras": self.codigo_barras,
+            "custo": self.custo,
+            "preco_venda": self.preco_venda,
+            "fornecedor": self.fornecedor,
+            "categoria": self.categoria
+        }
+
+        self.produtos_cadastrados.append(produto_info)
+        print("Produto cadastrado com sucesso!")
+
+
 
 if __name__ == '__main__':
     pass
