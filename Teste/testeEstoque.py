bead40cbac596a20db01ebaf52d1753e1a35f175
Josué Teixeira <zjosuez@outlook.com>
2023-12-03 13:21:45 -0300

    triangulação cadastrar estoque

--- Teste/testeEstoque.py
+++ Teste/testeEstoque.py
@@ -11,8 +11,12 @@ class TestEstoque(unittest.TestCase):
 
     def test_cadastrar_estoque(self):
         self.estoque = Estoque(self.produto, 10, "2022-01-01", "2022-01-31", "entrada")
+        self.estoque2 = Estoque(self.produto, 10, "2022-01-01", "2022-01-31", "entrada")
+        self.estoque3 = Estoque(self.produto, 10, "2022-01-01", "2022-01-31", "entrada")
         self.estoque.cadastrar_estoque()
-        self.assertEqual(len(self.estoque.estoque_cadastrado), 1)
+        self.estoque2.cadastrar_estoque()
+        self.estoque3.cadastrar_estoque()
+        self.assertEqual(len(self.estoque.estoque_cadastrado), 3)
 
 if __name__ == '__main__':
     unittest.main()
