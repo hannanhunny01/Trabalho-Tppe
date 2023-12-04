16e7ea74da24aa69417c8d42f20a48e8d510bedb
LeviQ27 <leviqueiroz647@gmail.com>
2023-12-04 00:01:39 -0300

    refactor fazer_transacao for Transacao

--- Models/Transacao.py
+++ Models/Transacao.py
@@ -14,8 +14,14 @@ class Transacao:
             "fornecedor": self.fornecedor,
             "loja": self.loja
         }
-
         self.transacoes_gravadas.append(transacao)
+
+        if self.fornecedor.estoques.qnt_produto >= 0 and self.loja.estoques.qnt_produto >= 0:
+            self.fornecedor.estoques.qnt_produto = self.fornecedor.estoques.qnt_produto - self.quantidade
+
+            self.loja.estoques.qnt_produto = self.loja.estoques.qnt_produto + self.quantidade
+        
+
         print("Transacao realizado com sucesso!")
 
         
