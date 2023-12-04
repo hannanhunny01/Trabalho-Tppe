d50de00ec744841845ad7c11609c48e341ba993e
heitormsb <heitormsb@gmail.com>
2023-12-03 22:20:08 -0300

    exceptions

--- Models/Exceptions.py
+++ Models/Exceptions.py
@@ -1,2 +1,11 @@
 class CodigoDeBarrasException(Exception):
     pass
+
+class DescricaoEmBrancoException(Exception):
+    pass
+
+class ValorInvalidoException(Exception):
+    pass
+
+class EstoqueNegativoException(Exception):
+    pass
