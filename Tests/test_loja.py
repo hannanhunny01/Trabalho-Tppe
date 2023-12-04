66151148763cce0b3ab5f97644bd2209c093d16c
luisgaboardi <luis.ggaboardi@gmail.com>
2023-12-03 23:42:32 -0300

    Refactor Loja

--- Tests/test_loja.py
+++ Tests/test_loja.py
@@ -1,47 +1,82 @@
-from Loja import Loja
-import sys
 import unittest
-sys.path.append('../Models')
+from Loja import Loja
 
+import unittest
+from Loja import Loja
 
 class TestLoja(unittest.TestCase):
 
-    def setUp(self):
-        self.loja = Loja("Loja Teste", "123456789",
-                         "Rua Teste, 123", "Representante Teste", [], [])
-
     def test_cadastrar_loja(self):
-        self.assertEqual(len(self.loja.lojas_cadastradas), 1)
+        loja = Loja(
+            nome="Loja Teste",
+            cnpj="123456789",
+            endereco="Rua Teste, 123",
+            representante="123",
+            estoques=[],
+            funcionarios=[]
+        )
+
+        # Teste de cadastro bem-sucedido
+        self.assertEqual(len(loja.listar_lojas()), 0)
+        self.assertTrue(loja.cadastrar_loja())
+        self.assertEqual(len(loja.listar_lojas()), 1)
+
+        # Tentativa de cadastrar a mesma loja novamente
+        self.assertFalse(loja.cadastrar_loja())
+
+        # Tentativa de cadastrar uma loja com nome vazio
+        loja.nome = ""
+        self.assertFalse(loja.cadastrar_loja())
 
     def test_remover_loja(self):
-        self.loja.remover_loja("123456789")
-        self.assertEqual(len(self.loja.lojas_cadastradas), 0)
+        loja = Loja(
+            nome="Loja Teste",
+            cnpj="123456789",
+            endereco="Rua Teste, 123",
+            representante="123",
+            estoques=[],
+            funcionarios=[]
+        )
+
+        self.assertTrue(loja.remover_loja())
+        self.assertEqual(len(loja.listar_lojas()), 0)
 
     def test_editar_loja(self):
-        self.loja.editar_loja("123456789", {"nome": "Loja Teste 2"})
-        self.assertEqual(
-            self.loja.lojas_cadastradas[0]["nome"], "Loja Teste 2")
+        loja = Loja(
+            nome="Loja Teste",
+            cnpj="123456789",
+            endereco="Rua Teste, 123",
+            representante="123",
+            estoques=[],
+            funcionarios=[]
+        )
 
-    def test_listar_lojas(self):
-        self.assertEqual(self.loja.listar_lojas(), self.loja.lojas_cadastradas)
+        # Tentativa de editar uma loja que não existe
+        self.assertFalse(loja.editar_loja("987654321", {"endereco": "Nova Rua, 456"}))
 
-    def test_buscar_loja_nome(self):
-        self.assertEqual(self.loja.buscar_loja_nome(
-            "Loja Teste"), self.loja.lojas_cadastradas[0])
+        # Cadastrar a loja e depois editá-la
+        loja.cadastrar_loja()
+        self.assertEqual(loja.listar_lojas()[0]["endereco"], "Rua Teste, 123")
+        self.assertTrue(loja.editar_loja("123456789", {"endereco": "Nova Rua, 456"}))
+        self.assertEqual(loja.listar_lojas()[0]["endereco"], "Nova Rua, 456")
 
-    def test_associar_funcionario(self):
-        self.loja.cadastrar_loja()
-        self.loja.associar_funcionario("123456789")
-        self.assertEqual(self.loja.lojas_cadastradas[0]["funcionarios"], ["123456789"])
+    def test_obter_loja_por_nome(self):
+        loja = Loja(
+            nome="Loja Teste",
+            cnpj="123456789",
+            endereco="Rua Teste, 123",
+            representante="123",
+            estoques=[],
+            funcionarios=[]
+        )
 
-    def test_desassociar_funcionario(self):
-        self.loja.desassociar_funcionario("123456789")
-        self.assertEqual(self.loja.lojas_cadastradas[0]["funcionarios"], [])
+        # Tentativa de obter uma loja que não existe
+        self.assertFalse(loja.obter_loja_por_nome("Loja Inexistente"))
 
-    def test_associar_representante(self):
-        self.loja.associar_representante("123456789")
-        self.assertEqual(self.loja.lojas_cadastradas[0]["representante"], "123456789")
-
+        # Cadastrar a loja e depois tentar obter por nome
+        loja.cadastrar_loja()
+        loja_obtida = loja.obter_loja_por_nome("Loja Teste")
+        self.assertEqual(loja_obtida["nome"], "Loja Teste")
 
 if __name__ == '__main__':
     unittest.main()
