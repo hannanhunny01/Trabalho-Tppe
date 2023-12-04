33fe64b6000b8da5a3f4aa81558ce5922c9f2b1d
LeviQ27 <leviqueiroz647@gmail.com>
2023-12-03 23:17:14 -0300

    create Loja_ex from Loja

--- Models/Transacao.py
+++ Models/Transacao.py
@@ -1,9 +1,24 @@
 class Transacao:
+    transacoes_gravadas = []
+
     def __init__(self, produto, quantidade, fornecedor, loja):
         self.produto = produto
         self.quantidade = quantidade
         self.fornecedor = fornecedor
         self.loja = loja
+    
+    def fazer_transacao(self):
+        transacao = {
+            "produto": self.produto,
+            "quantidade": self.quantidade,
+            "fornecedor": self.fornecedor,
+            "loja": self.loja
+        }
+
+        self.transacoes_gravadas.append(transacao)
+        print("Transacao realizado com sucesso!")
+
+        
 
 if __name__ == '__main__':
     pass
