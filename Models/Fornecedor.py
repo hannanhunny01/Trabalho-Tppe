29fa67d7e3dae8e84fe30fe950ac68cec19a89c5
Victor Eduardo <victorear05@gmail.com>
2023-12-03 22:16:50 -0300

    write remover categoria

--- Models/Fornecedor.py
+++ Models/Fornecedor.py
@@ -19,3 +19,13 @@ class Fornecedor:
         novo_fornecedor = Fornecedor(self.nome)
         Fornecedor.fornecedores_cadastrados.append(novo_fornecedor)
         print(f"Fornecedor '{self.nome}' cadastrado com sucesso!")
+
+    def remover_fornecedor(self, codigo_fornecedor):
+        for fornecedor in self.fornecedores_cadastrados:
+            if fornecedor["codigo_fornecedor"] == codigo_fornecedor:
+                Fornecedor.fornecedores_cadastrados.remove(fornecedor)
+                print(f"Fornecedor com código {codigo_fornecedor} removido com sucesso.")
+                return True
+        
+        print(f"Fornecedor com código {fornecedor} não encontrado.")
+        return False
