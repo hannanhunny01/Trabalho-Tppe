b917bda6a2bbbbf7ea531cbcb10e7c34b10190b1
Victor Eduardo <victorear05@gmail.com>
2023-12-03 21:58:05 -0300

    write cadastrar fornecedor

--- Models/Fornecedor.py
+++ Models/Fornecedor.py
@@ -1,3 +1,21 @@
 class Fornecedor:
+    
+    fornecedores_cadastrados = []
+
     def __init__(self, nome):
         self.nome = nome
+
+    def cadastrar_fornecedor(self):
+        
+        for fornecedor in Fornecedor.fornecedores_cadastrados:
+            if fornecedor.nome == self.nome:
+                print(f"O fornecedor '{self.nome}' já existe!")
+                return False
+
+        if self.nome.strip() == "":
+            print("Nome do fornecedor não pode ser vazio!")
+            return False
+
+        novo_fornecedor = Fornecedor(self.nome)
+        Fornecedor.fornecedores_cadastrados.append(novo_fornecedor)
+        print(f"Fornecedor '{self.nome}' cadastrado com sucesso!")
