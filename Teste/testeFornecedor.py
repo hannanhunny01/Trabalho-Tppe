975376ac14f639cb587ade1db3b36fa88440114b
Victor Eduardo <victorear05@gmail.com>
2023-12-03 21:49:58 -0300

    triangulacao test fornecedor

--- Teste/testeFornecedor.py
+++ Teste/testeFornecedor.py
@@ -11,8 +11,12 @@ class TestFornecedor(unittest.TestCase):
 
     def test_init_fornecedor(self):
         fornecedor = Fornecedor(nome="Indústrias Stark")
+        fornecedor1 = Fornecedor(nome="LexCorp")
+        fornecedor2 = Fornecedor(nome="Starlab")
 
         self.assertEqual(fornecedor.nome, "Indústrias Stark")
+        self.assertEqual(fornecedor1.nome, "LexCorp")
+        self.assertEqual(fornecedor2.nome, "Starlab")
 
 
 if __name__ == '__main__':
